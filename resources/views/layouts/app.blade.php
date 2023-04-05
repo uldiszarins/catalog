6e526f0107623e6a5d93a928a70d9c11518fc962
Uldis <uldis.zarins@zaao.lv>
2023-04-05 08:14:01 +0300

    Laravel 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ZAAO API</title>
+    <title>Katalogs</title>
 
     <!-- Scripts -->
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
