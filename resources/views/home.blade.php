29ead3f78d0dcda690ecedd7706a4997dbdc357d
Uldis <uldis.zarins@zaao.lv>
2023-04-05 10:38:41 +0300

    Theme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-
-        </div>
+    <div class="row">
+        asdfasd<br />
+        dfasfgdf
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,41 +25,58 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <div id="app">
-
-        <div class=" navbar-light bg-white shadow-sm">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <a class="navbar-brand  ms-1" href="{{ url('/') }}">
-                            Katalogs
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link disabled">
-
-
-                                </a>
-                            </li>
-                            <li class="nav-item">
-
-                            </li>
-                        </ul>
-
-                    </div>
-                </div>
-            </div>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Katalogs</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <input class="form-control form-control-dark w-100" type="text" placeholder="Meklēt" aria-label="Search">
+        <div class="navbar-nav">
         </div>
+    </header>
 
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/add">
+                                <span data-feather="home"></span>
+                                Pievienot
+                            </a>
+                        </li>
+                    </ul>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+                    <h6
+                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Saved reports</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
+                            <span data-feather="plus-circle"></span>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file-text"></span>
+                                Current month
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                @yield('content')
+            </main>
+        </div>
     </div>
 </body>
 
